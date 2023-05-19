ec3dfbb050738582ae1c075093e3c36e021787d4
Alex Fabian Melo <alexfabianmelo12@hotmail.com>
2023-05-19 18:11:18 -0500

    Funcion Login_Page

--- Ejercicios_Modulo_5_POM/Ejercicios_Modulo_5_POM/Test1.py
+++ Ejercicios_Modulo_5_POM/Ejercicios_Modulo_5_POM/Test1.py
@@ -7,8 +7,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from Ejercicios_Modulo_5_POM.Funciones import Funciones_Globales # 1. Se hace un import de Funciones.py en la raiz de la carpeta Ejercicios_Modulo_5_POM
+from Ejercicios_Modulo_5_POM.Page_Login import Pagina_Login # 2. Se hace un import de Page_Login.py en la raiz de la carpeta Ejercicios_Modulo_5_POM
 
-t = 1 # 2. Nos sirve si queremos que se ejecute de forma más rapida la prueba, solo con cambiar el valor de t cambia en todos
+t=1
 
 class baseTest(unittest.TestCase):
     def setUp(self):
@@ -18,11 +19,9 @@ class baseTest(unittest.TestCase):
 
     def test_POM(self): # 3. Es importante que se añada el "test_name" porque sino nos va a dar error
         driver = self.driver
-        f = Funciones_Globales(driver) # 4. Estas 2 primeras lineas siempre se deben hacer, para que se vinculen con el Funciones.py
-        f.Navegar("https://www.saucedemo.com/",t)
-        f.TextoByXPath("//input[@id='user-name']","MrBlueSky123",t)
-        f.TextoByID("password", "Alfamego#123456", t)
-        f.ClickByXPath("//input[@id='login-button']",t)
+        f = Funciones_Globales(driver) # 4. Se importan los driver tanto de Funciones_Globales como el Pagina_Login
+        pg = Pagina_Login(driver)
+        pg.Login_Master("https://www.saucedemo.com/","MrBlueSky123","Alfamego#123456",t)
 
     def tearDown(self):
         driver = self.driver
---
+++ Ejercicios_Modulo_5_POM/Page_Login.py
@@ -0,0 +1,25 @@
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from Ejercicios_Modulo_5_POM.Funciones import Funciones_Globales
+
+class Pagina_Login (): # 1. Se crea la clase Pagina_Login y se crea la inicialización dentro de esta
+    def __init__(self,driver):
+        self.driver = driver
+
+    def Login_Master(self,url,username,password,t):
+        driver = self.driver
+        f = Funciones_Globales(driver)  # 2. Estas 2 primeras lineas siempre se deben hacer, para que se vinculen con el Funciones.py
+        f.Navegar(url, t)
+        f.TextoByXPath("//input[@id='user-name']", username, t)
+        f.TextoByID("password", password, t)
+        f.ClickByXPath("//input[@id='login-button']", t)
+        return t
