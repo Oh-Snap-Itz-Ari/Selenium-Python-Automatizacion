74fa539dfb41390547e72618fec02daab3af7507
Alex Fabian Melo <alexfabianmelo12@hotmail.com>
2023-05-20 18:27:13 -0500

    Conexion con Excel

---
+++ Ejercicios_Modulo_6_Excel/Ejercicios_Modulo_6_Excel/Test1.py
@@ -0,0 +1,52 @@
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from Ejercicios_Modulo_6_Excel.Funciones import Funciones_Globales
+from Ejercicios_Modulo_6_Excel.Funciones_Excel import Funciones_Excel
+
+t=1
+
+class baseExcel(unittest.TestCase):
+    def setUp(self):
+        ser = Service("C:\Drivers\chromedriver.exe")
+        op = webdriver.ChromeOptions()
+        self.driver = webdriver.Chrome(service=ser, options=op)
+
+    def test_Excel(self): # 1. Es importante que se añada el "test_name" porque sino nos va a dar error
+        driver = self.driver
+        fg = Funciones_Globales(driver)
+        fe = Funciones_Excel(driver)
+        fg.Navegar("https://demoqa.com/text-box",t)
+        ruta = "C:/Users/alexf/Desktop/Automatización Selenium Python/Ejercicios/Ejercicios_Modulo_6_Excel/docs/Datos_Excel.xlsx"
+        filas = fe.getRowCount(ruta,"Hoja1") # 2. Se almacena dentro de la variable y luego se llama a través de la función
+
+        for r in range (2,filas+1): # 3. Se empieza desde 2 ya que la primera es de los nombres de los valores
+            name = fe.readData(ruta,"Hoja1",r,1) # 4. Toma el valor del nombre del Excel y lo guarda dentro de la variable
+            email = fe.readData(ruta,"Hoja1",r,2)
+            addr1 = fe.readData(ruta,"Hoja1",r,3) # 5. Se va moviendo campos hacia la derecha y los va almacenando en las variables
+            addr2 = fe.readData(ruta,"Hoja1",r,4)
+
+            fg.TextoMixto("XPATH","//input[@id='userName']",name,t)
+            fg.TextoMixto("XPATH","//input[@id='userEmail']",email,t)
+            fg.TextoMixto("XPATH", "//textarea[@id='currentAddress']", addr1, t)
+            fg.TextoMixto("XPATH", "//textarea[@id='permanentAddress']", addr2, t)
+            fg.ClickMixto("XPATH","//button[@id='submit']",t)
+
+            e = fg.FindExistMixto("XPATH","//p[@id='email']",t) # 6. Busca el correo a través de su XPATH y si existe lo indica
+
+            if (e == "Existe"): #7. Valida el valor que retorna la función
+                print("\n Se escribió de forma satisfactoria en el archivo que se encuentra en la ruta\n-> " +ruta)
+                # 8. Se especifica que en el campo 5 se debe escribir el mensaje correspondiente tras hacer una inserción
+                fe.writeData(ruta,"Hoja1",r,5,"Se insertó de forma satisfactoria")
+
+            else:
+                print("\n No se insertó de forma satisfactoria")
+                fe.writeData(ruta, "Hoja1", r, 5, "Error al realizar la inserción de la información")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Ejercicios_Modulo_6_Excel/Funciones.py
@@ -0,0 +1,279 @@
+import time
+import unittest
+import openpyxl # 1. Importante añadir la libreria
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+class Funciones_Globales (): # 2. Se crea la clase Funciones_Excel y se crea la inicialización dentro de esta
+    def __init__(self,driver):
+        self.driver = driver
+
+    # Función que permite navegar a una URL en concreto
+    def Navegar(self,url,tiempo):
+        self.driver.get(url) # 3. Espera a que se le envie url a través de la función Navegar
+        self.driver.maximize_window()
+        print("\nPágina abierta de forma satisfactoria:\n"+str(url))
+        t = time.sleep(tiempo)
+        return t
+
+    # Función que permite escribir texto, lo anterior a través de la busqueda del selector (XPATH o ID)
+    def TextoMixto(self,tipo,selector,texto,tiempo):
+        if (tipo == "XPATH"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, selector))) # 4. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();", val) # 5. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.XPATH, (selector)) # 6. Selecciona el elemento a través de su XPath
+                val.clear()  # 7. Permite limpiar el campo por si tiene algún valor escrito
+                val.send_keys(texto)  # 8. Recibe el valor de texto en la función y lo escribe sobre el elemento
+                print("\n- Escribiendo en el campo con {} ({}) el texto -> {}".format(tipo,selector,texto))
+                t = time.sleep(tiempo)  # 9. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return t
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return t
+
+        elif(tipo == "ID"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, selector)))  # 10. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 11. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.ID, (selector))  # 12. Selecciona el elemento a través de su XPath
+                val.clear()  # 13. Permite limpiar el campo por si tiene algún valor escrito
+                val.send_keys(texto)  # 14. Recibe el valor de texto en la función y lo escribe sobre el elemento
+                print("\n- Escribiendo en el campo con {} ({}) el texto -> {}".format(tipo,selector,texto))
+                t = time.sleep(tiempo)  # 15. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return t
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return t
+
+    # Función que permite hacer clic en un elemento en concreto, lo anterior a través de la busqueda del selector (XPATH o ID)
+    def ClickMixto(self,tipo,selector,tiempo):
+        if (tipo == "XPATH"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, selector))) # 13. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();", val) # 14. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.XPATH, (selector)) # 15. Selecciona el elemento a través de su XPath
+                val.click()
+                print("\n- Se da click en el campo con {} -> ({})".format(tipo,selector))
+                t = time.sleep(tiempo)  # 16. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return t
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return t
+
+        elif (tipo == "ID"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, selector)))  # 13. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 14. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.ID, (selector))  # 15. Selecciona el elemento a través de su XPath
+                val.click()
+                print("\n- Se da click en el campo con {} -> ({})".format(tipo, selector))
+                t = time.sleep(tiempo)  # 16. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return t
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return t
+
+    # Función que permite hacer la busqueda y selección de un elemento de una lista desplegable, lo anterior a través de
+    # su elemento (XPATH o ID)
+    def Select_Mixto_Text(self, tipo, selector, text, tiempo):
+        if (tipo == "XPATH"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, selector)))  # 13. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 14. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.XPATH, (selector))  # 15. Selecciona el elemento a través de su XPath
+                val = Select(val)
+                val.select_by_visible_text(text) # 16. Busca y selecciona el valor de text en el listado
+                print("\n- El campo seleccionado es -> ({})".format(text))
+                t = time.sleep(tiempo)  # 17. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return t
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return t
+
+        elif (tipo == "ID"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, selector)))  # 13. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 14. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.ID, (selector))  # 15. Selecciona el elemento a través de su XPath
+                val = Select(val)
+                val.select_by_visible_text(text) # 16. Busca y selecciona el valor de text en el listado
+                print("\n- El campo seleccionado es -> ({})".format(text))
+                t = time.sleep(tiempo)  # 17. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return t
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return t
+
+    # Función que permite hacer la busqueda y selección de un elemento de una lista desplegable (text, index o value),
+    # lo anterior a través de su elemento (XPATH o ID)
+    def Select_Mixto_Type(self, tipo, selector, type, dato, tiempo):
+        if (tipo == "XPATH"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, selector)))  # 13. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 14. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.XPATH, (selector))  # 15. Selecciona el elemento a través de su XPath
+                val = Select(val)
+
+                if(type == "text"):
+                    val.select_by_visible_text(dato)  # 16. Busca y selecciona el valor de text en el listado
+
+                elif(type == "index"):
+                    val.select_by_index(dato)
+
+                elif(type == "value"):
+                    val.select_by_value(dato)
+
+                print("\n- Se realizó la busqueda a través de ({}) y el campo seleccionado es equivalente a -> ({})".format(type,dato))
+                t = time.sleep(tiempo)  # 17. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return t
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return t
+
+        elif (tipo == "ID"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, selector)))  # 13. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 14. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.ID, (selector))  # 15. Selecciona el elemento a través de su XPath
+                val = Select(val)
+
+                if (type == "text"):
+                    val.select_by_visible_text(dato)  # 16. Busca y selecciona el valor de text en el listado
+
+                elif (type == "index"):
+                    val.select_by_index(dato)
+
+                elif (type == "value"):
+                    val.select_by_value(dato)
+
+                print("\n- Se realizó la busqueda a través de ({}) y el campo seleccionado es equivalente a -> ({})".format(type,dato))
+                t = time.sleep(tiempo)  # 17. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return t
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return t
+
+    # Función que permite subir un archivo especifico de una ruta del computador, lo anterior filtrando el campo a través de
+    # su elemento (XPATH o ID)
+    def Upload_Mixto(self, tipo, selector, ruta, tiempo):
+        if (tipo == "XPATH"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, selector)))  # 13. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 14. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.XPATH, (selector))  # 15. Selecciona el elemento a través de su XPath
+                val.send_keys(ruta)
+
+                print("\n- Se carga el archivo de forma satisfactoria a través de la busqueda en la ruta -> ({})".format(ruta))
+                t = time.sleep(tiempo)  # 17. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return t
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return t
+
+        elif (tipo == "ID"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, selector)))  # 13. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 14. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.ID, (selector))  # 15. Selecciona el elemento a través de su XPath
+                val.send_keys(ruta)
+
+                print("\n- Se carga el archivo de forma satisfactoria a través de la busqueda en la ruta -> ({})".format(ruta))
+                t = time.sleep(tiempo)  # 17. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return t
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return t
+
+    # Esta función permite hacer click en varios campos de un formulario, sin importar la cantidad de argumentos que se envie
+    def Clic_Multiple_Mixto(self, tipo, tiempo, *args):
+        if (tipo == "XPATH"):
+            try:
+                for num in args: # 13. Recorre la cantidad de argumentos y se almacena este valor dentro de num
+                    val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, num)))  # 13. Busca el elemento
+                    val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 14. Si lo encuentra va hacia el elemento
+                    val = self.driver.find_element(By.XPATH, (num))  # 15. Selecciona el elemento a través de su XPath
+                    val.click()
+                    print("\n- Se da click en el campo con ({}) -> ({})".format(tipo,num))
+                    t = time.sleep(tiempo)  # 16. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                    return t
+
+            except TimeoutException as ex:
+                for num in args:
+                    print(ex.msg)
+                    print("\n- No se encontró el elemento:\n- " + num)
+
+        elif (tipo == "ID"):
+            try:
+                for num in args:  # 13. Recorre la cantidad de argumentos y se almacena este valor dentro de num
+                    val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, num)))  # 13. Busca el elemento
+                    val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 14. Si lo encuentra va hacia el elemento
+                    val = self.driver.find_element(By.ID, (num))  # 15. Selecciona el elemento a través de su XPath
+                    val.click()
+                    print("\n- Se da click en el campo con ({}) -> ({})".format(tipo,num))
+                    t = time.sleep(tiempo)  # 16. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                    return t
+
+            except TimeoutException as ex:
+                for num in args:
+                    print(ex.msg)
+                    print("\n- No se encontró el elemento:\n- " + num)
+
+    # Función que valida si el nombre se cargo de forma correcta tras dar clic en el botón y lo muestra en el resultado por pantalla
+    def FindExistMixto(self, tipo, selector, tiempo):
+        if (tipo == "XPATH"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, selector)))  # 13. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 14. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.XPATH, (selector))  # 15. Selecciona el elemento a través de su XPath
+                print("\n- El elemento de tipo ({}) y nombre ({}) -> Existe".format(tipo, selector))
+                t = time.sleep(tiempo)  # 16. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return "Existe" # 17. Retorna el valor de que se encontró el elemento y por ende existe
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return "No existe"
+
+        elif (tipo == "ID"):
+            try:
+                val = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, selector)))  # 13. Busca el elemento
+                val = self.driver.execute_script("arguments[0].scrollIntoView();",val)  # 14. Si lo encuentra va hacia el elemento
+                val = self.driver.find_element(By.ID, (selector))  # 15. Selecciona el elemento a través de su XPath
+                print("\n- El elemento de tipo ({}) y nombre ({}) -> Existe".format(tipo, selector))
+                t = time.sleep(tiempo)  # 16. Recibe el valor de tiempo y lo implementa una vez que se escribe el texto
+                return "Existe"  # 17. Retorna el valor de que se encontró el elemento y por ende existe
+
+            except TimeoutException as ex:
+                print(ex.msg)
+                print("\n- No se encontró el elemento:\n- " + selector)
+                return "No existe" # 18. Retorna el valor de que no se encontró el elemento y por ende NO existe
+
+    # Función que brinda un mensaje de finalización exitoso
+    def Salida(self):
+        print("\nLa prueba ha sido finalizada de forma satisfactoria.")
